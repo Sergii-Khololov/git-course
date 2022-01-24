0ab4b7971bcf6e6bbda9e0c7a151f0a1eb7aaf0a
Sergii-Khololov <serhololov@gmail.com>
2022-01-25 00:24:29 +0100

    rebase master

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
+document.body.innerHTML = "<h1>Hello from React 10</h1>";
 
-document.body.innerHTML = '<h1>Hello from React 10</h1>'
-
-console.log('Git push  !!!!!!!!!! :>> ');
+console.log("Git push  !!!!!!!!!! :>> ");
